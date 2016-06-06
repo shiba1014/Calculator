fd4b528694639a5630f26f655265bda145f01314
shiba1014 <shiba.png@gmail.com>
2016-06-07 00:11:52 +0900

    add auto layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var operationLabel: UILabel!
     var number1: Int = 0
     var number2: Int = 0
     var operation: Int = 0
@@ -18,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        operationLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,7 +29,75 @@ class ViewController: UIViewController {
 
 
     @IBAction func pushedNum(id: AnyObject) {
-        resultLabel.text = String(id.tag)
+        number1 = id.tag
+        resultLabel.text = String(number1)
+    }
+    
+    @IBAction func pushedZero() {
+        number1 = number1 * 10
+        resultLabel.text = String(number1)
+    }
+    
+    @IBAction func pushedOperator(id: AnyObject) {
+        operation = id.tag
+        number2 = number1
+        number1 = 0
+        switch operation {
+        case 1:
+            //puls
+            operationLabel.text = "+"
+            return
+        case 2:
+            //minus
+            operationLabel.text = "-"
+            return
+        case 3:
+            //multiply
+            operationLabel.text = "×"
+            return
+        case 4:
+            //divide
+            operationLabel.text = "÷"
+            return
+        default:
+            return
+        }
+
+    }
+    
+    @IBAction func pushedEqual() {
+        switch operation {
+        case 1:
+            //puls
+            resultLabel.text = String(number1 + number2)
+            number2 = number1 + number2
+            return
+        case 2:
+            //minus
+            resultLabel.text = String(number2 - number1)
+            number2 = number2 - number1
+            return
+        case 3:
+            //multiply
+            resultLabel.text = String(number1 * number2)
+            number2 = number1 * number2
+            return
+        case 4:
+            //divide
+            resultLabel.text = String(number2 / number1)
+            number2 = number2 / number1
+            return
+        default:
+            return
+        }
+    }
+    
+    @IBAction func pushedAC() {
+        resultLabel.text = "0"
+        operationLabel.text = ""
+        number1 = 0
+        number2 = 0
+        operation = 0
     }
 }
 
